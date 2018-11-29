33124f3597e251f153a162a4496c880093df80e8
griffinwalkerMM <griffin@metismachine.com>
2018-11-29 09:28:56 -0500

    Bump up resources, remove validation for nwo

--- text_classifier.py
+++ text_classifier.py
@@ -1,17 +1,28 @@
 import turicreate as tc
+import save_models as sm
+from skafossdk import *
+
+ska = Skafos()
 
 # Load data
-data = tc.SFrame('yelp-data.csv')
+data = tc.SFrame('https://static.turi.com/datasets/regression/yelp-data.csv')
+
 
 # Create a model
 model = tc.text_classifier.create(data, 'stars', features=['text'])
 
 # Make predictions & evaluation the model
-predictions = model.predict(data)
-results = model.evaluate(data)
+#predictions = model.predict(data)
+#results = model.evaluate(data)
 
-# Save the model for later use in Turi Create
-model.save('MyTextClassifier.model')
+# export to coreml
+coreml_model_name = "text_classifier.mlmodel"
+res = model.export_coreml(coreml_model_name)
 
-# Export for use in Core ML
-model.export_coreml('MySentenceClassifier.mlmodel')
+# compress the model
+compressed_model_name, compressed_model = sm.compress_model(coreml_model_name)
+
+# save to Skafos
+sm.skafos_save_model(skafos = ska, model_name = compressed_model_name,
+								compressed_model = compressed_model,
+								permissions = 'public')
